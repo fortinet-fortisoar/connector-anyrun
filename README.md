5cc5d8c6dfd24e621ebb9a2b9a8b5b63a3e37e9a
Semen Shalnev <85073445+semayellow@users.noreply.github.com>
2026-01-14 18:56:22 +0300

    feat(general): implement sandbox integration

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # connector-anyrun
-ANY.RUN connector is an integration with https://any.run/ an online Sandboxing service which allows you to performs actions like submit file for analysis, get history, get report
-
-Documentation available [here](https://docs.fortinet.com/document/fortisoar/1.1.0/any-run/348/any-run-v1-1-0)
+ANY.RUN empowers SOC teams to cut MTTD/MTTR with fast verdicts using the Interactive Sandbox. 
+Add the ANY.RUN Cloud Sandbox connector as a step in FortiSOAR™ playbooks and perform automated operations to achieve: 
+- **Faster triage**: Submit files/URLs for instant analysis across Windows, Linux, Android. Get answers in seconds to catch threats that beat standard defenses and increase the detection rate by up to 36%.
+- **Streamlined workflows**: Retrieve reports (JSON, HTML, IOCs) directly in FortiSOAR without tool-switching. Act without delays to stop attacks before they have a chance to hurt your infrastructure. 
---
+++ anyrun/utils.py
@@ -0,0 +1,30 @@
+from anyrun.connectors import SandboxConnector
+from anyrun.connectors.sandbox.operation_systems import (
+    AndroidConnector,
+    LinuxConnector,
+    WindowsConnector,
+)
+
+from .constants import VERSION
+
+
+def get_windows_sandbox_connector(config: dict) -> WindowsConnector:
+    """ Builds ANY.RUN Sandbox instance for the Windows OS """
+    return SandboxConnector().windows(config.get('api_key'), integration=VERSION, verify_ssl=config.get('verify_ssl'))
+
+
+def get_linux_sandbox_connector(config: dict) -> LinuxConnector:
+    """ Builds ANY.RUN Sandbox instance for the Linux OS """
+    return SandboxConnector().linux(config.get('api_key'), integration=VERSION, verify_ssl=config.get('verify_ssl'))
+
+
+def get_android_sandbox_connector(config: dict) -> AndroidConnector:
+    """ Builds ANY.RUN Sandbox instance for the Android OS """
+    return SandboxConnector().android(config.get('api_key'), integration=VERSION, verify_ssl=config.get('verify_ssl'))
+
+
+os_connector_mapping = {
+    'windows': get_windows_sandbox_connector,
+    'linux': get_linux_sandbox_connector,
+    'android': get_android_sandbox_connector,
+}
